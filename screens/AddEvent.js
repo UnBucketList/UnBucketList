f393cb7c338f7c4d9c686fa2465125f2b5827767
RToddBuckner <tdwolf6@gmail.com>
2020-07-22 13:39:17 -0700

    added conditional to addEvent.js to require event name and details and notify user when input is missing

--- screens/AddEvent.js
+++ screens/AddEvent.js
@@ -27,12 +27,6 @@ const AddEvent = (props) => {
   let event_name, description, location, date, guests;
 
   const handleAddEvent = () => {
-    // if (!event_name || !description) {
-    //   // alert('You must set an event name and description');
-    //   return;
-    // } else if (!date) {
-    //   date = null;
-    // }
     const event = {
       event_name,
       description,
