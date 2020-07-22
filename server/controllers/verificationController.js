4e0f9804efabdbaaee49736fafd39872ddbb46f6
Sean <smith.seanm@yahoo.com>
2020-07-21 19:19:13 -0700

    create user and verify user both working for signup and login w/ bcrypt

--- server/controllers/verificationController.js
+++ server/controllers/verificationController.js
@@ -5,33 +5,63 @@ const db = require("./../db/db");
 const verificationController = {};
 
 verificationController.createUser = (req, res, next) => {
-  console.log('made it to create user')
-  const { name, email, username, password } = req.body;
+  console.log("made it to create user");
+  let { name, email, username, password } = req.body;
+  username = username.toLowerCase();
   let string = `
   INSERT INTO users (name, email, username, password)
-  VALUES ($1, $2, $3, $4)
+  VALUES ($1, $2, $3, $4);
   `;
 
-  bcrypt.hash(password, parseInt(process.env.SALT), function(err, hash) {
-    if (err){
-      console.log('ERROR IN BCRYPT')
+  bcrypt.hash(password, parseInt(process.env.SALT), function (err, hash) {
+    if (err) {
+      console.log("ERROR IN BCRYPT");
       return next(err);
     }
     const values = [name, email, username, hash];
     db.query(string, values)
-      .then((query) => {
+      .then((result) => {
         return next();
       })
       .catch((err) => {
-        console.log('error in dbquery')
+        console.log("error in dbquery");
         console.log(err);
         return next(err);
       });
+  });
+};
 
-  })
-}
-  
+verificationController.verifyUser = (req, res, next) => {
+  console.log("made it to verify user");
+  let { username, password } = req.body;
+  username = username.toLowerCase();
+  const values = [username];
+  let string = `
+  SELECT password FROM users WHERE username=$1;
+  `;
+  db.query(string, values)
+    .then((result) => {
+      bcrypt.compare(password, result.rows[0].password, function (
+        err,
+        pwMatch
+      ) {
+        if (err) {
+          console.log("error in bcrypt compare");
+          return next(err);
+        }
+        if (pwMatch) {
+          return next();
+        } else {
+          return next({ log: "incorrect password" });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log("err in verifyUser");
+      return next(err);
+    });
+};
+
 //res.locals.user_id
-
 
 module.exports = verificationController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const userRouter = express.Router();
-const verificationController = require('../controllers/verificationController');
+const verificationController = require("../controllers/verificationController");
 
-userRouter.get('/', (req, res) => {
-  return res.status(200).json("userRouter")
+userRouter.get("/", (req, res) => {
+  return res.status(200).json("userRouter");
 });
 
 userRouter.post(
-  '/signup',
-   verificationController.createUser,
-   //probably need to send to eventController to get all events
-   (req, res) => {
-     console.log('yay it worked')
-     return res.status(404).json('working SON');
-   }
+  "/signup",
+  verificationController.createUser,
+  //probably need to send to eventController to get all events
+  (req, res) => {
+    console.log("yay it worked");
+    return res.status(404).json("working SON");
+  }
+);
 
-  )
-
+userRouter.post("/login", verificationController.verifyUser, (req, res) => {
+  return res.status(404).json("worked");
+});
 
 module.exports = userRouter;
