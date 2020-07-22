7d4214c68aa1c98c150d61dcd0d6059ca998ad5a
RToddBuckner <tdwolf6@gmail.com>
2020-07-22 12:24:51 -0700

    update  for new pull from truth

--- screens/AddEvent.js
+++ screens/AddEvent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -22,6 +22,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const AddEvent = (props) => {
+  const [nameValueEntered, setNameValueEntered] = useState(true);
   let event_name, description, location, date, guests;
 
   const handleAddEvent = () => {
@@ -32,27 +33,38 @@ const AddEvent = (props) => {
       date,
       guests,
     };
-    props.addEvent(event, props.username);
-    props.navigation.navigate('Home');
+    if(event.event_name === undefined || event.description === undefined){
+      setNameValueEntered(false);
+      // alert('Please include event Name and Description');
+      console.log('hey hey hey')
+    } else{
+      props.addEvent(event, props.username);
+      props.navigation.navigate('Home');
+    }
+    
   };
 
   return (
-    <View style={styles.container}>
+     <View style={styles.container}>
       <TextInput
         value={event_name}
         onChangeText={(e) => {
           event_name = e;
         }}
-        placeholder={'Event Name'}
-        style={styles.input}
+        placeholder={nameValueEntered ? 'Event Name' : 'Event Name Required'}
+        style={nameValueEntered ? styles.input : styles.noInput}
+        // style={ styles.input}
+        // placeholderTextColor={nameValueEntered ? 'gray' : 'red'}
       />
       <TextInput
         value={description}
         onChangeText={(e) => {
           description = e;
         }}
-        placeholder={'Details'}
-        style={styles.input}
+        placeholder={nameValueEntered ? 'Details': "Event Details Required"}
+        style={nameValueEntered ? styles.input : styles.noInput}
+        // style={styles.input}
+        // placeholderTextColor={nameValueEntered ? 'gray' : 'red'}
       />
       <TextInput
         value={location}
@@ -99,6 +111,16 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     borderColor: 'gray',
+    borderRadius: 3,
+    marginBottom: 10,
+  },
+  noInput: {
+    placeholderTextColor: 'red',
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'red',
     borderRadius: 3,
     marginBottom: 10,
   },
